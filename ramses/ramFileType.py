bc14ba056fe65fc4819f7395fa638791f53716dc
Jeanrifi <jpluchez@gmail.com>
2021-05-07 11:53:57 +0200

    add def fromDict() in ramFileType

--- ramses/ramFileType.py
+++ ramses/ramFileType.py
@@ -4,6 +4,17 @@ class RamFileType( RamObject ):
     """A File type.
     File types are typically used with RamPipe."""
 
+    @staticmethod
+    def fromDict( fileTypeDict ):
+        """Builds a RamFileType from dict like the ones returned by the RamDaemonInterface"""
+
+        f = RamFileType(
+            fileTypeDict['name'],
+            fileTypeDict['shortName'],
+            fileTypeDict['extensions']
+        )
+        return f
+
     def __init__(self, name, shortname, extensions ):
         """
         Args:
