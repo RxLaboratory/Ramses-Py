09d5e4733a80db1da43a9886317bbef81743c0b5
Duduf <git@duduf.com>
2021-05-07 13:19:08 +0200

    versionFilePath implementation

--- ramses/ramItem.py
+++ ramses/ramItem.py
@@ -133,9 +133,14 @@ class RamItem( RamObject ):
             # check if successful
             if daemon.checkReply( replyDict ):
                 content = replyDict['content']
-                status = RamStatus( state=content['state'], user=content['user'], comment=content['comment'],
-                                    version=content['version'], stateDate=content['date'],
-                                    completionRatio=content['completionRatio'])
+                status = RamStatus(
+                    state=content['state'],
+                    comment=content['comment'],
+                    completionRatio=content['completionRatio'],
+                    version=content['version'],
+                    stateDate=content['date'],
+                    user=content['user'],
+                    )
 
                 return status
 
@@ -144,9 +149,6 @@ class RamItem( RamObject ):
         if currentVersionPath == None:
             log( "There was an error getting the latest version or none was found." )
             return None
-
-        currentVersionPath = self.folderPath() + '/' + currentVersionPath
-        currentVersionPath = Ramses.instance().currentProject().absolutePath( currentVersionPath )
 
         currentStatus = RamStatus.getFromPath( currentVersionPath )
 
@@ -235,14 +237,11 @@ class RamItem( RamObject ):
             int
         """
         # Check args, return shortName (str) or "" or raise TypeError:
-        getObjectShortName( step )
-        getObjectShortName( resource )
-        getObjectShortName( stateId )
+        step = getObjectShortName( step )
 
         # Uniquement pour test
         # abc = Ramses.instance.daemonInterface().getCurrentStatus("SEA", "Sea", "ASSET")
         # print(abc)
-
 
         listWithState = []
  
@@ -282,7 +281,6 @@ class RamItem( RamObject ):
                     highestVersion = versionInt
 
             return highestVersion
-
 
         # Else check in the folders
         if self.folderPath == '':
@@ -477,16 +475,16 @@ class RamItem( RamObject ):
             str
         """
         # Check args, return shortName (str) or "" or raise TypeError:
-        getObjectShortName( step )
+        step = getObjectShortName( step )
 
         folderPath = self.folderPath( step )
-        if folderPath != None:
+        if folderPath != "":
             return RamFileManager.buildPath( ( folderPath, RamSettings.instance().folderNames.versions ) )
         else:
             return ""
 
-    def versionFilePath( self, step, resource="" ): #TODO if online
-        """Latest version file path relative to the item root folder
+    def versionFilePath( self, step, resource="" ):
+        """Latest version file path
 
         Args:
             step (RamStep or str)
@@ -496,66 +494,46 @@ class RamItem( RamObject ):
             str
         """
         # Check args, return shortName (str) or "" or raise TypeError:
-        getObjectShortName( step )
-        getObjectShortName( resource )
+        step = getObjectShortName( step )
 
-        # If we're online, ask the client
-        if Ramses.instance().online():
-            #TODO ask the client
-            return None
-        
-        # Else check in the folders
-        # It is basically the same as getLatestVersion. Only difference is that it does not take the stateId into account
-        # and returns the path instead of the version number.
-        if self.folderPath == '':
-            print( "The given item has no folderPath." )
-            return None
+        versionFolderPath = self.versionFolderPath( step )
 
-        folderPath = Ramses.instance().currentProject().absolutePath( self.folderPath )
+        if not os.path.isdir( versionFolderPath ):
+            return ""
 
-        if not os.path.isdir( folderPath ):
-            print( "The given item's folder was not found.\nThis is the path that was checked:\n" + folderPath )
-            return None
-
-        baseName = os.path.basename( self.folderPath() ) + '_' + step # Name without the resource str (added later)
-        stepFolderPath = folderPath + '/' + baseName
-
-        if os.path.isdir( stepFolderPath ) == False:
-            print( "The folder for the following step: " + step + " has not been found." )
-            return None
-        if os.path.isdir( stepFolderPath + '/_versions' ) == False:
-            print( "ramses_versions directory has not been found" )
-            return None
-
-        foundFiles = os.listdir( stepFolderPath + '/_versions' )
+        foundFiles = os.listdir( versionFolderPath )
         highestVersion = 0
-        highestVersionFileName = ""
+        highestVersionFileName = ''
 
         for foundFile in foundFiles:
-            if not os.path.isfile( stepFolderPath + '/_versions/' + foundFile ): # This is in case the user has created folders in ramses_versions
+            # In case the user has created folders in ramses_versions
+            if not os.path.isfile( stepFolderPath + '/_versions/' + foundFile ): 
+                continue
+            # In case other assets have been misplaced here
+            if not foundFile.startswith( baseName ):
                 continue
 
             decomposedFoundFile = RamFileManager.decomposeRamsesFileName( foundFile )
 
-            if decomposedFoundFile == None:
-                continue
-            if not foundFile.startswith( baseName ): # In case other assets have been misplaced here
+            if decomposedFoundFile is None:
                 continue
             if decomposedFoundFile[ "resourceStr" ] != resource:
                 continue
-            if decomposedFoundFile[ "version" ] == '':
+            if decomposedFoundFile[ "version" ] < 0:
                 continue
             
-            versionInt = int( decomposedFoundFile["version"] )
+            versionInt = decomposedFoundFile["version"]
             if versionInt > highestVersion:
                 highestVersion = versionInt
                 highestVersionFileName = foundFile
 
         if highestVersionFileName == '':
-            print( "No version was found" )
-            return None
+            return ''
 
-        highestVersionFilePath = baseName + "/_versions/" + highestVersionFileName
+        highestVersionFilePath = RamFileManager.buildPath((
+            versionFolderPath,
+            highestVersionFileName
+        ))
 
         return highestVersionFilePath
 
