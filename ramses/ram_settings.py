4cdb2043cdc7d18bb92133ff34dbefdf719e8d82
Duduf <git@duduf.com>
2021-10-24 16:08:36 +0200

    renamed version to alpha

--- ramses/ram_settings.py
+++ ramses/ram_settings.py
@@ -23,7 +23,7 @@ import json
 from .constants import FolderNames, LogLevel
 from .logger import log
 
-theVersion = "0.2.0"
+theVersion = "0.2.0-alpha"
 
 class RamSettings( object ):
     """Gets and saves settings used by Ramses.
