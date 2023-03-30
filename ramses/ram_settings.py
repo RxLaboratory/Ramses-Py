488d8bed893d105ae8ce1494d77dfe42ece5e67a
Duduf <git@duduf.com>
2023-03-30 15:25:44 +0200

    bump version to 0.8.4

--- ramses/ram_settings.py
+++ ramses/ram_settings.py
@@ -23,7 +23,7 @@ import json
 from .constants import FolderNames, LogLevel
 from .logger import log
 
-theVersion = "0.8.3-Beta"
+theVersion = "0.8.4-Beta"
 
 class RamSettings( object ):
     """Gets and saves settings used by Ramses.
