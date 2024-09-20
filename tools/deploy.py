f2d41f785284906b4107d3ebdd6e127bc3fca9a8
Duduf <git@duduf.com>
2024-09-20 11:30:51 +0200

    improve deploy

--- tools/deploy.py
+++ tools/deploy.py
@@ -1,14 +1,14 @@
 import os
 import rxbuilder.utils as utils
 import rxbuilder.py as py
+import shutil
 from _config import (
     REPO_PATH,
     BUILD_PATH,
 )
 
-
 VERSION = utils.read_version(REPO_PATH)
-MOD_PATH = os.path.join( BUILD_PATH, 'ramses' )
+MOD_PATH = os.path.join( BUILD_PATH, 'ramses-py' )
 SRC_PATH = os.path.join( REPO_PATH, 'ramses' )
 
 def build():
@@ -17,12 +17,27 @@ def build():
 
     py.build_folder(
         SRC_PATH,
-        MOD_PATH
+        os.path.join( MOD_PATH, 'ramses' )
+    )
+
+    # Copy Readme and toml
+    shutil.copy(
+        os.path.join(REPO_PATH, 'README.md'),
+        os.path.join(MOD_PATH, 'README.md')
+    )
+    toml = os.path.join(MOD_PATH, 'pyproject.toml')
+    shutil.copy(
+        os.path.join(REPO_PATH, 'pyproject.toml'),
+        toml
     )
 
     # Update metadata
+    utils.replace_in_file( {
+        '#version#': VERSION,
+    }, toml)
+
     config_file = os.path.join(
-        MOD_PATH, 'ram_settings.py'
+        MOD_PATH, 'ramses', 'ram_settings.py'
     )
     utils.replace_in_file( {
         '#version#': VERSION,
@@ -33,7 +48,7 @@ def zip_module():
         BUILD_PATH,
         'ramses-py_' + VERSION + '.zip'
     )
-    utils.zip_dir(BUILD_PATH, zip_file)
+    utils.zip_dir(MOD_PATH, zip_file)
 
 if __name__ == '__main__':
     utils.wipe(BUILD_PATH)
